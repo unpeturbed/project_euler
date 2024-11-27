32e5d51068685d99022e827b6a2b85d8a6804aa9
unpeturbed <ayodeledrust@gmail.com>
2024-11-27 14:49:50 +0100

    Separate functions into appropriate files

--- mult_of_3_5/src/main.rs
+++ mult_of_3_5/src/main.rs
@@ -2,19 +2,8 @@
 //that are multiples of 3 or 5, we get 3,5,6 and 9
 //The sum of these multiples is 23
 //Find the sum of all the multiples of 3 or 5 below 1000
-
+use mult_of_3_5::mult;
 fn main() {
-    let numbers = 1..1000;
-    let mut multiples = Vec::new();
-
-    for number in numbers {
-        if number % 3 == 0 || number % 5 == 0 {
-            multiples.push(number);
-        }
-    }
-    let mut sum = 0;
-    for multiple in &multiples {
-        sum = sum + multiple;
-    }
+    let sum = mult(1, 1000);
     println!("{sum}");
 }
