1d50938e7c4c10233457e0fc98b472bf2844b209
unpeturbed <ayodeledrust@gmail.com>
2024-12-28 18:12:18 +0100

    Add question

--- in_rust/smallest_multiple/src/main.rs
+++ in_rust/smallest_multiple/src/main.rs
@@ -1,3 +1,8 @@
+ // 2520 is the smallest number that can be divided by each of the numbers from 1
+ //to 10 without any remainder.
+//What is the smallest positive number that is evenly divisible by all of the numbers from
+//1 to 20?
+
 fn gcd(a: u64, b: u64) -> u64 {
     if b == 0 {
         a
